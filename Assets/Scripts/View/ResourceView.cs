dd0486d7b8623d502eb53b6d057e77efed93c03d
KonH <konh@yandex.ru>
2020-11-04 15:54:03 +0700

    Add value change counters

--- Assets/Scripts/View/ResourceView.cs
+++ Assets/Scripts/View/ResourceView.cs
@@ -1,3 +1,4 @@
+using System;
 using Game.ViewModel;
 using TMPro;
 using UniRx;
@@ -7,15 +8,35 @@ namespace Game.View {
 	public sealed class ResourceView : MonoBehaviour {
 		[SerializeField] string   _name;
 		[SerializeField] TMP_Text _text;
+		[SerializeField] TMP_Text _appearText;
+
+		long _lastAmount;
 
 		public void Init(ResourcePackViewModel viewModel) {
 			var property = viewModel.Resources[_name];
+			UpdateValue(property.Value);
+			StopAppear();
 			property
+				.Skip(1)
+				.Do(StartAppear)
+				.Delay(TimeSpan.FromSeconds(0.75))
+				.Do(_ => StopAppear())
 				.Subscribe(UpdateValue);
+		}
+
+		void StartAppear(long newAmount) {
+			var diff = (newAmount - _lastAmount);
+			_appearText.text = $"{((diff >= 0) ? "+" : "")}{diff}";
+			_appearText.enabled = true;
+		}
+
+		void StopAppear() {
+			_appearText.enabled = false;
 		}
 
 		void UpdateValue(long newAmount) {
 			_text.text = newAmount.ToString();
+			_lastAmount = newAmount;
 		}
 	}
 }
